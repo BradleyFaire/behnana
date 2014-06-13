33afafe1ffb608a2015203d99e07dbfc9097ae7d
Emma Gerlach <emma.gerlach@gmail.com>
2014-06-13 17:39:26 +1200

    Updated index, created basic nav

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
 <?php
+
+require_once '../libraries/cart.lib.php';
+require_once '../libraries/config.lib.php';
+require_once '../libraries/database.lib.php';
+require_once '../libraries/form.lib.php';
+require_once '../libraries/hash.lib.php';
+require_once '../libraries/login.lib.php';
+require_once '../libraries/model.lib.php';
+
+
+
+include '../views/header.php';
+include '../views/navigation.php';
+include '../views/page.php';
+include '../views/footer.php';
---
+++ views/navigation.php
@@ -0,0 +1,31 @@
+<nav>
+
+	<ul>
+
+		<li>Fruit
+			<ul>
+				<li>Bananas</li>
+				<li>Coconuts</li>
+				<li>Pineapple</li>
+				<li>Mangoes</li>
+			</ul>
+		</li>
+		<li>Fish
+			<ul>
+				<li>Snapper</li>
+				<li>Lobster</li>
+				<li>Mussels</li>
+			</ul>
+		</li>
+		<li>Rum
+			<ul>
+				<li>White rum</li>
+				<li>Dark rum</li>
+				<li>Coconut rum</li>
+			</ul>
+		</li>
+
+	</ul>
+
+</nav>
+
